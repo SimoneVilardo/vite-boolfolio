3ecbb683d2777e80545a5c51b1ed5ed0eedcf7d3
SimoneVilardo <simovila95@gmail.com>
2023-08-29 14:47:44 +0200

    creazione script app main

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,6 @@ export default{
   </div>
 </template>
 
-<style>
-
+<style lang="scss">
+  @use'./styles/generals.scss' as *;
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,11 +1,40 @@
 <script>
-export default{
+import axios from 'axios';
 
+export default{
+    name: 'AppMain',
+    data(){
+        return{
+            baseUrl: 'http://localhost:8000',
+            projects: [],
+            loading: true
+        }
+    },
+    created(){
+        this.getProjects();
+    },
+    methods:{
+        getProjects(){
+            this.loading = true;
+            axios.get(`${this.baseUrl}/api/projects`).then((response) =>{
+                if(response.data.success){
+                    this.projects = response.data.results;
+                    this.loading = false;
+                }
+            })
+        }
+    }
 }
 </script>
 
 <template>
-  ciao vite
+  <div class="container">
+    <div class="row">
+        <div class="col-12">
+            <h1 class="text-center"></h1>
+        </div>
+    </div>
+  </div>
 </template>
 
 <style>
