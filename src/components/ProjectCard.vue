497f8d5ef0c1a9f9388a6d6182c47b1cc0f1edfe
SimoneVilardo <simovila95@gmail.com>
2023-08-30 15:38:31 +0200

    bonus completato

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,52 @@
+<script>
+import { store } from '../store.js'
+export default {
+    name:"ProjectCard",
+    props: {
+        project : Object
+    },
+    data(){
+        return{
+            store,
+            maxNumCharacters: 60,
+        }
+    },
+    methods: {
+        truncateText(text){
+            if(text.length > this.maxNumCharacters){
+                return text.substr(0,this.maxNumCharacters) + '...';
+            }
+
+            return text;
+        }
+    },
+}
+</script>
+<template lang="">
+    <div class="card my-3 min-height-200px">
+            <div class="card-image-top">
+                <img :src="`${this.store.baseUrl}/storage/${project.image}`" class="img-fluid">
+            </div>
+            <div class="card-body">
+                <div class="card-title">
+                    {{ project.title }}
+                </div>
+                <p>
+                    <span v-if="project.type"><strong>{{ project.type.name }}</strong></span>
+                    <span v-else><strong>Tipo non assegnato</strong></span>
+                </p>
+                <p v-if="project.technologies">
+                    <span class="badge text-bg-primary me-3" v-for="technology in project.technologies" :key="technology.id">
+                        {{ technology.name }}
+                    </span>
+                </p>
+                <p>{{ truncateText(project.content) }}</p>
+            </div>
+            <div class="card-footer">
+                <a href="#" class="btn btn-sm btn-primary"> Leggi il progetto</a>
+            </div>
+        </div>
+</template>
+<style lang="">
+    
+</style>
--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -1,18 +1,19 @@
 <script>
 import AppLoader from '../components/AppLoader.vue';
+import ProjectCard from '../components/ProjectCard.vue';
 import axios from 'axios';
 import { store } from '../store.js';
 
 export default{
     name: 'ProjectList',
     components:{
-        AppLoader
+        AppLoader,
+        ProjectCard
     },
     data(){
         return{
             store,
             projects: [],
-            maxNumCharacters: 60,
             currentPage: 1,
             lastPage: null
         }
@@ -30,75 +31,46 @@ export default{
             this.store.loading = false;
         })},
 
-        truncateText(text){
-            if(text.length > this.maxNumCharacters){
-                return text.substr(0,this.maxNumCharacters) + '...';
-            }
-
-            return text;
-        }
     }
 }
 </script>
 
 <template>
-  <div class="container">
-    <div class="row">
-        <div class="col-12">
-            <h1 class="text-center">BOOLFOLIO</h1>
-        </div>
-    </div>
-  </div>
-  <AppLoader v-if="store.loading" />
-  <div v-else class="container">
-    <div class="row">
-        <div class="col-12 col-md-4" v-for="project in projects" :key="project.id">
-            <div class="card my-3 min-height-200px">
-                <div class="card-image-top">
-                    <img :src="`${this.store.baseUrl}/storage/${project.image}`" class="img-fluid">
-                </div>
-                <div class="card-body">
-                    <div class="card-title">
-                        {{ project.title }}
-                    </div>
-                    <p>
-                        <span v-if="project.type"><strong>{{ project.type.name }}</strong></span>
-                        <span v-else><strong>Tipo non assegnato</strong></span>
-                    </p>
-                    <p v-if="project.technologies">
-                        <span class="badge text-bg-primary me-3" v-for="technology in project.technologies" :key="technology.id">
-                            {{ technology.name }}
-                        </span>
-                    </p>
-                    <p>{{ truncateText(project.content) }}</p>
-                </div>
-                <div class="card-footer">
-                    <a href="#" class="btn btn-sm btn-primary"> Leggi il progetto</a>
-                </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="text-center">BOOLFOLIO</h1>
             </div>
         </div>
     </div>
-  </div>
-  <div class="container mt-5">
-    <div class="row">
-        <div class="col-12">
-            <nav class="d-flex justify-content-center">
-                <ul class="pagination">
-                    <li :class="currentPage === 1 ? 'disabled' : ''">
-                        <button class="page-link" @click="getProjects(currentPage - 1)">
-                            Precedente
-                        </button>
-                    </li>
-                    <li :class="currentPage === lastPage ? 'disabled' : ''">
-                        <button class="page-link" @click="getProjects(currentPage + 1)">
-                            Successivo
-                        </button>
-                    </li>
-                </ul>
-            </nav>
+    <AppLoader v-if="store.loading" />
+    <div v-else class="container">
+        <div class="row">
+            <div class="col-12 col-md-4" v-for="project in projects" :key="project.id">
+            <ProjectCard :project="project"/>
+            </div>
         </div>
     </div>
-  </div>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-12">
+                <nav class="d-flex justify-content-center">
+                    <ul class="pagination">
+                        <li :class="currentPage === 1 ? 'disabled' : ''">
+                            <button class="page-link" @click="getProjects(currentPage - 1)">
+                                Precedente
+                            </button>
+                        </li>
+                        <li :class="currentPage === lastPage ? 'disabled' : ''">
+                            <button class="page-link" @click="getProjects(currentPage + 1)">
+                                Successivo
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss">
