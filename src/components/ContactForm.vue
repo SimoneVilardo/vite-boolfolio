563652d8ce7c0e811b574d46ec423fc75ef1fe20
SimoneVilardo <simovila95@gmail.com>
2023-09-01 14:30:30 +0200

    [ADD] file ContactForm con rel name+data+methods

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,48 @@
+<script>
+import axios from 'axios';
+import { store } from '../store';
+export default {
+    name: 'ContactForm',
+    data(){
+        return{
+            store,
+            name: '',
+            email: '',
+            messagge: '',
+            success: false,
+            errors: {}
+        }
+    },
+    methods: {
+        sendForm(){
+            const data = {
+                name: this.name,
+                email: this.email,
+                content: this.message
+            }
+
+            this.errors = {};
+
+            axios.post(`${this.store.baseUrl}/api/contacts`, data).then((response) => {
+                this.success = response.data.success;
+                if(!this.success){
+                    this.errors = response.data.errors;
+                }
+                else{
+                    this.name = '';
+                    this.email = '';
+                    this.messagge = '';
+                }
+            })
+        }
+    },
+}
+</script>
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<style lang="">
+    
+</style>
